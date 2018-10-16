38ef0d329d82534a56adbcabaf1f153b71beb2e4
Harsh Bandhey <30626026+raptor419@users.noreply.github.com>
2018-10-16 07:59:07 +0530

    Update data.py

--- data.py
+++ data.py
@@ -7,7 +7,11 @@ opcode_table = {
 	"cla":"0000", "lac":"0001", "sac":"0010","add":"0011", 
 	"sub":"0100","brz":"0101","brn":"0110", "brp":"0111", 
 	"inp":"1000", "dsp":"1001", "mul":"1010", "div":"1011", 
-	"stp":"1100","start":"ad","dc":"dl","ds":"dl","end":"ad"
+	"stp":"1100","start":"ad","dc":"dl","ds":"dl","end":"ad",
+	"CLA":"0000", "LAC":"0001", "SAC":"0010","ADD":"0011", 
+	"SUB":"0100","BRZ":"0101","BRN":"0110", "BRP":"0111", 
+	"INP":"1000", "DSP":"1001", "MUL":"1010", "DIV":"1011", 
+	"STP":"1100","START":"ad","DC":"dl","DS":"dl","END":"ad"
 }
 
 reg_table = {
@@ -24,7 +28,7 @@ literal_table = {
 	
 }
 
-error_type = ["invalid opcode","undefined symbol"]
+error_type = ["invalid opcode","undefined symbol","duplicate label"]
 
 error_list = list()
 
